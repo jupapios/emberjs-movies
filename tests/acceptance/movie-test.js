8f3287f892a28785f8fb4ef3bdfe52163f75cbac
Juan Pablo Pinilla Ossa <jpinilla@climate.com>
2014-09-08 13:19:42 -0500

    Acceptance test: movie
    - Movie page should
      - show the title, score, release date
      - contain a 'main page' link

---
+++ tests/acceptance/movie-test.js
@@ -0,0 +1,53 @@
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+import { goToMovie } from './utils/actions';
+
+var App;
+
+module('Acceptance: Movie', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('it contains the details', function() {
+  expect(3);
+
+  goToMovie('First Blood');
+  andThen(function() {
+    var title = find('.qa-title').text().trim();
+    var score = find('.qa-score').text().trim();
+    var date = find('.qa-date').text().trim();
+
+    equal(title, 'First Blood');
+    equal(score, '89%');
+    equal(date, '05/27/2014');
+  });
+
+});
+
+test('it contains the Main page link', function() {
+  expect(2);
+
+  goToMovie();
+  andThen(function() {
+    var homeLink = find('.qa-home');
+    equal(homeLink.length, 1);
+    equal(homeLink.text().trim(), 'Main page');
+  });
+});
+
+test('main page link goes to the home', function() {
+  expect(3);
+
+  goToMovie();
+  click('.qa-home');
+  andThen(function() {
+    equal(currentRouteName(), 'index');
+    equal(currentPath(), 'index');
+    equal(currentURL(), '/');
+  });
+});
